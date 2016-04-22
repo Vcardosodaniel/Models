9300aecf9e7fb811c6b2d3e8b0f7a7b7a3e16e9f
Vcardosodaniel <vcardoso.daniel@gmail.com>
2016-04-22 09:14:20 -0300

    Config Itenspedidovenda

--- cadpedidovenda.js
+++ cadpedidovenda.js
@@ -1,61 +1,61 @@
-var cadpedidovenda = sequelize.define ('cadpedidovenda', {
+var pedidovenda = sequelize.define ('pedidovenda', {
 
-	CODIGO: {
+	ID: {
 		type: Sequelize.INTEGER,
 		field: 'CODIGO',
 		primaryKey: true,
 		allowNull: false
 	},
 
-	CODIGOCOMANDA: {
+	ORDER_CODE: {
 		type: Sequelize.INTEGER,
 		field: 'CODIGOCOMANDA'
 	},
 
-	CLIENTE: {
+	CLIENT: {
 		type: Sequelize.INTEGER,
 		field: 'CLIENTE',
 		allowNull: false
 	},
 
-	CONFIRMADO: {
+	CONFIRMED: {
 		type: Sequelize.INTEGER,
 		field: 'CONFIRMADO'
 	},
 
-	VENDEDOR: {
+	SELLER: {
 		type: Sequelize.INTEGER,
 		field: 'VENDEDOR',
 		allowNull: false
 	},
 
-	DATAORCAMENTO: {
+	ESTIMATE_DATE: {
 		type: Sequelize.DATE,
 		field: 'DATAORCAMENTO',
 		allowNull: false
 	},
 
-	DATAVALIDADE: {
+	VALIDATE_DATE: {
 		type: Sequelize.DATE,
 		field: 'DATAVALIDADE'
 	},
 
-	DESCONTO: {
+	DISCOUNT: {
 		type: Sequelize.DOUBLE,
 		field: 'DESCONTO'
 	},
 
-	DOCFISCAL: {
+	FISCAL_DOC: {
 		type: Sequelize.INTEGER,
 		field: 'DOCFISCAL'
 	},
 
-	TIPODOC: {
+	DOC_TYPE: {
 		type: Sequelize.STRING,
 		field: 'TIPODOC'
 	},
 
-	OBSERVACAO: {
+	OBS: {
 		type: Sequelize.BLOB,
 		field: 'OBSERVACAO'
 	},
@@ -70,75 +70,31 @@ var cadpedidovenda = sequelize.define ('cadpedidovenda', {
 		field: 'TOTAL'
 	},
 
-	IMPRESSO: {
+	IMPRESSED: {
 		type: Sequelize.TEXT('tiny'),
 		field: 'IMPRESSO'
 	},
 
-	CLIENTE_NOME: {
+	CLIENT_NAME: {
 		type: Sequelize.STRING,
 		field: 'CLIENTE_NOME'
 	},
 
-	FECHADO: {
+	CLOSED: {
 		type: Sequelize.TEXT('tiny'),
 		field: 'FECHADO'
 	},
 
-	DATA_ENTREGA: {
+	DELIVERY_DATA: {
 		type: Sequelize.DATE,
 		field: 'DATA_ENTREGA'
 	}, 
 
-	FORMA_PAGAMENTO: {
+	PAYMENT_FORM: {
 		type: Sequelize.TEXT('tiny'),
 		field: 'FORMA_PAGAMENTO'
 	}
 }, {
 	freezeTableName: true
-	},
-	{
-		getterMethods: {
-			getCODIGO: function() {return this.CODIGO};
-			getCODIGOCOMANDA: function() {return this.CODIGOCOMANDA};
-			getCONFIRMADO: function() {return this.CONFIRMADO};
-			getCLIENTE: function() {return this.CLIENTE};
-			getVENDEDOR: function() {return this.VENDEDOR};
-			getDATAORCAMENTO: function() {return this.DATAORCAMENTO};
-			getDATAVALIDADE: function() {return this.DATAVALIDADE};
-			getDESCONTO: function() {return this.DESCONTO};
-			getDOCFISCAL: function() {return this.DOCFISCAL};
-			getTIPODOC: function() {return this.TIPODOC};
-			getOBSERVACAO: function() {return this.OBSERVACAO};
-			getPED_ORC: function() {return this.PED_ORC};
-			getTOTAL: function() {return this.TOTAL};
-			getIMPRESSO: function() {return this.IMPRESSO};
-			getCLIENTE_NOME: function() {return this.CLIENTE_NOME};
-			getFECHADO: function() {return this.FECHADO};
-			getDATA_ENTREGA: function() {return this.DATA_ENTREGA};
-			getFORMA_PAGAMENTO: function() {return this.FORMA_PAGAMENTO};
-		},
-
-		setterMethods: {
-			setCODIGO: function(CODIGO) {return this.setDataValue('CODIGO')};
-			setCODIGOCOMANDA: function(CODIGOCOMANDA) {return this.setDataValue('CODIGOCOMANDA')};
-			setCONFIRMADO: function(CONFIRMADO) {return this.setDataValue('CONFIRMADO')};
-			setCLIENTE: function(CLIENTE) {return this.setDataValue('CLIENTE')};
-			setVENDEDOR: function(VENDEDOR) {return this.setDataValue('VENDEDOR')};
-			setDATAORCAMENTO: function(DATAORCAMENTO) {return this.setDataValue('DATAORCAMENTO')};
-			setDATAVALIDADE: function(DATAVALIDADE) {return this.setDataValue('DATAVALIDADE')};
-			setDESCONTO: function(DESCONTO) {return this.setDataValue('DESCONTO')};
-			setDOCFISCAL: function(DOCFISCAL) {return this.setDataValue('DOCFISCAL')};
-			setTIPODOC: function(TIPODOC) {return this.setDataValue('TIPODOC')};
-			setOBSERVACAO: function(OBSERVACAO) {return this.setDataValue('OBSERVACAO')};
-			setPED_ORC: function(PED_ORC) {return this.setDataValue('PED_ORC')};
-			setTOTAL: function(TOTAL) {return this.setDataValue('TOTAL')};
-			setIMPRESSO: function(IMPRESSO) {return this.setDataValue('IMPRESSO')};
-			setCLIENTE_NOME: function(CLIENTE_NOME) {return this.setDataValue('CLIENTE_NOME')};
-			setFECHADO: function(FECHADO) {return this.setDataValue('FECHADO')};
-			setDATA_ENTREGA: function(DATA_ENTREGA) {return this.setDataValue('DATA_ENTREGA')};
-			setFORMA_PAGAMENTO: function(FORMA_PAGAMENTO) {return this.setDataValue('FORMA_PAGAMENTO')};	
-		},
 	}
-
 );
--- itenspedidovenda.js
+++ itenspedidovenda.js
@@ -1,3 +1,102 @@
 var itenspedidovenda = sequelize.define('itenspedidovenda', {
-		
-});
+
+	ID: {
+		type: Sequelize.INTEGER,
+		field: 'CODIGO',
+		primaryKey: true,
+		allowNull: false
+	},
+
+	ORDER_NUM: {
+		type: Sequelize.INTEGER,
+		field: 'NUM_PEDIDO'
+	},
+
+	PRODUCT: {
+		type: Sequelize.INTEGER,
+		field: 'PRODUTO'
+	},
+
+	QUANTITY: {
+		type: Sequelize.DOUBLE,
+		field: 'QUANTIDADE'
+	},
+
+	AMOUNT: {
+		type: Sequelize.DOUBLE,
+		field: 'VALOR'
+	},
+
+	PAYMENT_TERM: {
+		type: Sequelize.DOUBLE,
+		field: 'VALORPRAZO'
+	},
+
+	IN_CASH: {
+		type: Sequelize.DOUBLE,
+		field: 'VALORVISTA'
+	},
+
+	DISCOUNT: {
+		type: Sequelize.DOUBLE,
+		field: 'DESCONTO'
+	},
+
+	DISCOUNT_TERM: {
+		type: Sequelize.DOUBLE,
+		field: 'DESCONTOPRAZO'
+	},
+
+	DISCOUNT_IN_CASH: {
+		type: Sequelize.DOUBLE,
+		field: 'DESCONTOVISTA'
+	},
+
+	CANCELED: {
+		type: Sequelize.TEXT('tiny'),
+		field: 'CANCELADO'
+	},
+
+	INCLUSION_DATE: {
+		type: Sequelize.DATE,
+		field: 'DT_INCLUSAO'
+	},
+
+	ALIQUOT_TYPE: {
+		type: Sequelize.STRING,
+		field: 'ALIQUOTA_TIPO'
+	},
+
+	ALIQUOT_VALUE: {
+		type: Sequelize.DOUBLE,
+		field: 'ALIQUOTA_VALOR'
+	},
+
+	ALIQUOT_INDEX: {
+		type: Sequelize.INTEGER,
+		field: 'ALIQUOTA_INDICE'
+	},
+
+	PRODUCT_DESC: {
+		type: Sequelize.STRING,
+		field: 'PRODUTO_DESCRICAO'
+	},
+
+	UNITY: {
+		type: Sequelize.STRING,
+		field: 'UNIDADE'
+	},
+
+	USER_CANC: {
+		type: Sequelize.INTEGER,
+		field: 'USUARIO_CANC'
+	},
+
+	USER_ENTRY: {
+		type: Sequelize.INTEGER,
+		field: 'USUARIO_LANC'
+	}
+}, {
+	freezeTableName: true
+	}
+);
