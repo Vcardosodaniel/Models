637360756d9c30f30b7bf8de49e7b13d4ca087e1
Vcardosodaniel <vcardoso.daniel@gmail.com>
2016-04-22 10:24:29 -0300

    Add Links.txt

--- itenspedidovenda.js
+++ itenspedidovenda.js
@@ -1,102 +1,105 @@
-var itenspedidovenda = sequelize.define('itenspedidovenda', {
+'use strict';
+module.exports = function(sequelize, DataTypes){
+	var itenspedidovenda = sequelize.define('itenspedidovenda', {
 
-	ID: {
-		type: Sequelize.INTEGER,
-		field: 'CODIGO',
-		primaryKey: true,
-		allowNull: false
-	},
+		ID: {
+			type: Sequelize.INTEGER,
+			field: 'CODIGO',
+			primaryKey: true,
+			allowNull: false
+		},
 
-	ORDER_NUM: {
-		type: Sequelize.INTEGER,
-		field: 'NUM_PEDIDO'
-	},
+		ORDER_NUM: {
+			type: Sequelize.INTEGER,
+			field: 'NUM_PEDIDO'
+		},
 
-	PRODUCT: {
-		type: Sequelize.INTEGER,
-		field: 'PRODUTO'
-	},
+		PRODUCT: {
+			type: Sequelize.INTEGER,
+			field: 'PRODUTO'
+		},
 
-	QUANTITY: {
-		type: Sequelize.DOUBLE,
-		field: 'QUANTIDADE'
-	},
+		QUANTITY: {
+			type: Sequelize.DOUBLE,
+			field: 'QUANTIDADE'
+		},
 
-	AMOUNT: {
-		type: Sequelize.DOUBLE,
-		field: 'VALOR'
-	},
+		AMOUNT: {
+			type: Sequelize.DOUBLE,
+			field: 'VALOR'
+		},
 
-	PAYMENT_TERM: {
-		type: Sequelize.DOUBLE,
-		field: 'VALORPRAZO'
-	},
+		PAYMENT_TERM: {
+			type: Sequelize.DOUBLE,
+			field: 'VALORPRAZO'
+		},
 
-	IN_CASH: {
-		type: Sequelize.DOUBLE,
-		field: 'VALORVISTA'
-	},
+		IN_CASH: {
+			type: Sequelize.DOUBLE,
+			field: 'VALORVISTA'
+		},
 
-	DISCOUNT: {
-		type: Sequelize.DOUBLE,
-		field: 'DESCONTO'
-	},
+		DISCOUNT: {
+			type: Sequelize.DOUBLE,
+			field: 'DESCONTO'
+		},
 
-	DISCOUNT_TERM: {
-		type: Sequelize.DOUBLE,
-		field: 'DESCONTOPRAZO'
-	},
+		DISCOUNT_TERM: {
+			type: Sequelize.DOUBLE,
+			field: 'DESCONTOPRAZO'
+		},
 
-	DISCOUNT_IN_CASH: {
-		type: Sequelize.DOUBLE,
-		field: 'DESCONTOVISTA'
-	},
+		DISCOUNT_IN_CASH: {
+			type: Sequelize.DOUBLE,
+			field: 'DESCONTOVISTA'
+		},
 
-	CANCELED: {
-		type: Sequelize.TEXT('tiny'),
-		field: 'CANCELADO'
-	},
+		CANCELED: {
+			type: Sequelize.TEXT('tiny'),
+			field: 'CANCELADO'
+		},
 
-	INCLUSION_DATE: {
-		type: Sequelize.DATE,
-		field: 'DT_INCLUSAO'
-	},
+		INCLUSION_DATE: {
+			type: Sequelize.DATE,
+			field: 'DT_INCLUSAO'
+		},
 
-	ALIQUOT_TYPE: {
-		type: Sequelize.STRING,
-		field: 'ALIQUOTA_TIPO'
-	},
+		ALIQUOT_TYPE: {
+			type: Sequelize.STRING,
+			field: 'ALIQUOTA_TIPO'
+		},
 
-	ALIQUOT_VALUE: {
-		type: Sequelize.DOUBLE,
-		field: 'ALIQUOTA_VALOR'
-	},
+		ALIQUOT_VALUE: {
+			type: Sequelize.DOUBLE,
+			field: 'ALIQUOTA_VALOR'
+		},
 
-	ALIQUOT_INDEX: {
-		type: Sequelize.INTEGER,
-		field: 'ALIQUOTA_INDICE'
-	},
+		ALIQUOT_INDEX: {
+			type: Sequelize.INTEGER,
+			field: 'ALIQUOTA_INDICE'
+		},
 
-	PRODUCT_DESC: {
-		type: Sequelize.STRING,
-		field: 'PRODUTO_DESCRICAO'
-	},
+		PRODUCT_DESC: {
+			type: Sequelize.STRING,
+			field: 'PRODUTO_DESCRICAO'
+		},
 
-	UNITY: {
-		type: Sequelize.STRING,
-		field: 'UNIDADE'
-	},
+		UNITY: {
+			type: Sequelize.STRING,
+			field: 'UNIDADE'
+		},
 
-	USER_CANC: {
-		type: Sequelize.INTEGER,
-		field: 'USUARIO_CANC'
-	},
+		USER_CANC: {
+			type: Sequelize.INTEGER,
+			field: 'USUARIO_CANC'
+		},
 
-	USER_ENTRY: {
-		type: Sequelize.INTEGER,
-		field: 'USUARIO_LANC'
-	}
-}, {
-	freezeTableName: true
-	}
-);
+		USER_ENTRY: {
+			type: Sequelize.INTEGER,
+			field: 'USUARIO_LANC'
+		}
+	}, {
+		freezeTableName: true
+		}
+	);
+}
--- pedidovenda.js
+++ pedidovenda.js
@@ -1,100 +1,104 @@
-var pedidovenda = sequelize.define ('pedidovenda', {
+'use strict';
+module.exports = function(sequelize, DataTypes){
+	var pedidovenda = sequelize.define ('pedidovenda', {
 
-	ID: {
-		type: Sequelize.INTEGER,
-		field: 'CODIGO',
-		primaryKey: true,
-		allowNull: false
-	},
+		ID: {
+			type: Sequelize.INTEGER,
+			field: 'CODIGO',
+			primaryKey: true,
+			allowNull: false
+		},
 
-	ORDER_CODE: {
-		type: Sequelize.INTEGER,
-		field: 'CODIGOCOMANDA'
-	},
+		ORDER_CODE: {
+			type: Sequelize.INTEGER,
+			field: 'CODIGOCOMANDA'
+		},
 
-	CLIENT: {
-		type: Sequelize.INTEGER,
-		field: 'CLIENTE',
-		allowNull: false
-	},
+		CLIENT: {
+			type: Sequelize.INTEGER,
+			field: 'CLIENTE',
+			allowNull: false
+		},
 
-	CONFIRMED: {
-		type: Sequelize.INTEGER,
-		field: 'CONFIRMADO'
-	},
+		CONFIRMED: {
+			type: Sequelize.INTEGER,
+			field: 'CONFIRMADO'
+		},
 
-	SELLER: {
-		type: Sequelize.INTEGER,
-		field: 'VENDEDOR',
-		allowNull: false
-	},
+		SELLER: {
+			type: Sequelize.INTEGER,
+			field: 'VENDEDOR',
+			allowNull: false
+		},
 
-	ESTIMATE_DATE: {
-		type: Sequelize.DATE,
-		field: 'DATAORCAMENTO',
-		allowNull: false
-	},
+		ESTIMATE_DATE: {
+			type: Sequelize.DATE,
+			field: 'DATAORCAMENTO',
+			allowNull: false
+		},
 
-	VALIDATE_DATE: {
-		type: Sequelize.DATE,
-		field: 'DATAVALIDADE'
-	},
+		VALIDATE_DATE: {
+			type: Sequelize.DATE,
+			field: 'DATAVALIDADE'
+		},
 
-	DISCOUNT: {
-		type: Sequelize.DOUBLE,
-		field: 'DESCONTO'
-	},
+		DISCOUNT: {
+			type: Sequelize.DOUBLE,
+			field: 'DESCONTO'
+		},
 
-	FISCAL_DOC: {
-		type: Sequelize.INTEGER,
-		field: 'DOCFISCAL'
-	},
+		FISCAL_DOC: {
+			type: Sequelize.INTEGER,
+			field: 'DOCFISCAL'
+		},
 
-	DOC_TYPE: {
-		type: Sequelize.STRING,
-		field: 'TIPODOC'
-	},
+		DOC_TYPE: {
+			type: Sequelize.STRING,
+			field: 'TIPODOC'
+		},
 
-	OBS: {
-		type: Sequelize.BLOB,
-		field: 'OBSERVACAO'
-	},
+		OBS: {
+			type: Sequelize.BLOB,
+			field: 'OBSERVACAO'
+		},
 
-	PED_ORC: {
-		type: Sequelize.INTEGER,
-		field: 'PED_ORC'
-	},
+		PED_ORC: {
+			type: Sequelize.INTEGER,
+			field: 'PED_ORC'
+		},
 
-	TOTAL: {
-		type: Sequelize.DOUBLE,
-		field: 'TOTAL'
-	},
+		TOTAL: {
+			type: Sequelize.DOUBLE,
+			field: 'TOTAL'
+		},
 
-	IMPRESSED: {
-		type: Sequelize.TEXT('tiny'),
-		field: 'IMPRESSO'
-	},
+		IMPRESSED: {
+			type: Sequelize.TEXT('tiny'),
+			field: 'IMPRESSO'
+		},
 
-	CLIENT_NAME: {
-		type: Sequelize.STRING,
-		field: 'CLIENTE_NOME'
-	},
+		CLIENT_NAME: {
+			type: Sequelize.STRING,
+			field: 'CLIENTE_NOME'
+		},
 
-	CLOSED: {
-		type: Sequelize.TEXT('tiny'),
-		field: 'FECHADO'
-	},
+		CLOSED: {
+			type: Sequelize.TEXT('tiny'),
+			field: 'FECHADO'
+		},
 
-	DELIVERY_DATA: {
-		type: Sequelize.DATE,
-		field: 'DATA_ENTREGA'
-	}, 
+		DELIVERY_DATA: {
+			type: Sequelize.DATE,
+			field: 'DATA_ENTREGA'
+		}, 
 
-	PAYMENT_FORM: {
-		type: Sequelize.TEXT('tiny'),
-		field: 'FORMA_PAGAMENTO'
-	}
-}, {
-	freezeTableName: true
-	}
-);
+		PAYMENT_FORM: {
+			type: Sequelize.TEXT('tiny'),
+			field: 'FORMA_PAGAMENTO'
+		}
+	}, {
+		freezeTableName: true
+		}
+	);
+	return pedidovenda;
+};
